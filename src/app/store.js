d395e86ddbd776c4faea2a10626c526216b4e0c4
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-22 21:28:23 +0430

    couterReducer state created and added to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import counterReducer from './CounterSlice';
 
 export const store = configureStore({
   reducer: {
