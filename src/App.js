5ff5af053c351c40ed7c0ac92490826577d08cda
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-24 12:04:53 +0430

    for store the new configureStore and for reducer the redux/toolkit createSlice used

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import HookCounter from './components/HookCounter';
+import newStore from './redux/newStore';
 // import {store} from './redux/store'
 
 function App() {
   return (
-    <Provider store={store}>
+    <Provider store={newStore}>
       <div className="App">
         <Counter />
         <HookCounter />
