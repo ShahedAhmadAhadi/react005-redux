da25badd9391de809062e9f3f37c83d25ebf383c
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-25 23:18:53 +0430

    the data fetched successfully with redux thunk and added to ui with loading and error states :

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,17 +3,12 @@ import { useSelector, useDispatch, connect} from 'react-redux'
 import { fetchUserRequest, fetchUsers } from '../redux'
 
 function Users(props) {
-    // const users = useSelector(state => state.user)
-    // const dispatch = useDispatch()
     useEffect(() => {
         props.fetchUsers()
     }, [])
 
     console.log(props, 'adlfjalfja')
-    return <> 
-    {/* <div onClick={props.fetchData()}>li</div> */}
-    {/* {props.userData.loading && <div>loading...</div>}
-    {props.userData.users && console.log(props.userData)} */}
+    return <>
     <div onClick={() => props.fetchUsers()}>li</div> {props.userData.loading ? (<h1>Loading...</h1>) : props.userData.error ? (<h2>{props.userData.error}</h2>) : (<><h2>User List</h2>{props.userData.users && props.userData.users.map(user =><ul key={user.id}>{user.username}</ul>)}</>)}
     </>
 }
