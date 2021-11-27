89a8220038c8bc979c52571f6a29ac6af8ea812e
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-27 22:09:08 +0430

    action for store is added and store subscribed and unsubscribed

--- src/index.js
+++ src/index.js
@@ -4,14 +4,53 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//       <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+// Omit existing React imports
+
+import {store} from './redux/store'
+
+// Log the initial state
+console.log('Initial state: ', store.getState())
+// {todos: [....], filters: {status, colors}}
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() =>
+  console.log('State after dispatch: ', store.getState())
+)
+
+// Now, dispatch some actions
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
+
+store.dispatch({ type: 'todos/todoToggled', payload: 0 })
+store.dispatch({ type: 'todos/todoToggled', payload: 1 })
+
+store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Active' })
+
+store.dispatch({
+  type: 'filters/colorFilterChanged',
+  payload: { color: 'red', changeType: 'added' }
+})
+
+// Stop listening to state updates
+unsubscribe()
+
+// Dispatch one more action to see what happens
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
+
+// Omit existing React rendering logic
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux'
 import counterReducer from './counter/counterReducer'
 import userReducer from './user/userReducer'
+import todoReducer from './todo/todoReducer'
+import filtersReducer from './filter/filterReducer'
 
 const rootReducer = combineReducers({
     counter : counterReducer,
-    user: userReducer
+    user: userReducer,
+    todos: todoReducer,
+    filters: filtersReducer
 })
 
 export default rootReducer
